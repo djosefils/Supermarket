d1eb2d71e93c8a0f73b89f35f7760c85a0fdc6a5
djosefils <youssef.benmessaoud3@docaposte.fr>
2021-01-28 17:08:41 +0100

    first commit

---
+++ src/main/java/supermarket/price/kata/SupermarketPriceApplication.java
@@ -0,0 +1,13 @@
+package supermarket.price.kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketPriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supermarket/price/kata/SupermarketPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supermarket.price.kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketPriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
